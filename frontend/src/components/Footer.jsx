74e220f623b06bd63bd34cdfa880160eba1bf8eb
Ali Ahmad Khan <150443989+khanaliahmad269@users.noreply.github.com>
2026-05-11 13:22:21 +0500

    Add frontend/src/components/Footer.jsx

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-gray-800 p-4 mt-8">
+      <div className="container mx-auto text-center text-white">
+        <p>&copy; 2023 KFC Clone. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
