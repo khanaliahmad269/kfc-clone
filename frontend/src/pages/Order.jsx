9e30c4e436a6578f8c112afcf980580a92ac7cd9
Ali Ahmad Khan <150443989+khanaliahmad269@users.noreply.github.com>
2026-05-11 13:22:28 +0500

    Add frontend/src/pages/Order.jsx

---
+++ frontend/src/pages/Order.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import OrderForm from '../components/OrderForm';
+
+function Order() {
+  return (
+    <div>
+      <OrderForm />
+    </div>
+  );
+}
+
+export default Order;
